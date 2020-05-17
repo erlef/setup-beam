1a26cf429fe51bd254fa0f63ea9418bc716acc95
Jonathan Clem <jclem@github.com>
2020-05-17 10:56:04 -0400

    Allow older Elixir versions in test mix.exs

--- test-project/mix.exs
+++ test-project/mix.exs
@@ -5,7 +5,7 @@ defmodule Test.MixProject do
     [
       app: :test,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
