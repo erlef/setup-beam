e0c9b41009ae943e7f116a4017d43de78b9ae3ac
Jonathan Clem <jclem@github.com>
2020-09-16 15:05:55 -0400

    Use plain assert testing

--- __tests__/setup-elixir.test.js
+++ __tests__/setup-elixir.test.js
@@ -1,18 +1,21 @@
 const {getElixirVersion} = require('../src/setup-elixir')
+const {deepStrictEqual} = require('assert')
 
-describe('getElixirVersion', () => {
-  test('actual version parsing', async () => {
-    const vsn = await getElixirVersion('v1.10.x', '23')
-    expect(vsn).toEqual(['v1.10.4', '23'])
-  })
+async function test() {
+  let vsn
+  vsn = await getElixirVersion('v1.10.x', '23')
+  deepStrictEqual(vsn, ['v1.10.4', '23'])
 
-  test('version range parsing', async () => {
-    const vsn = await getElixirVersion('^v1.10', '23')
-    expect(vsn).toEqual(['v1.10.4', '23'])
-  })
+  vsn = await getElixirVersion('^v1.10', '23')
+  deepStrictEqual(vsn, ['v1.10.4', '23'])
 
-  test('pre-release versions', async () => {
-    const vsn = await getElixirVersion('v1.11.0-rc.0', '23')
-    expect(vsn).toEqual(['v1.11.0-rc.0', '23'])
+  vsn = await getElixirVersion('v1.11.0-rc.0', '23')
+  deepStrictEqual(vsn, ['v1.11.0-rc.0', '23'])
+}
+
+test()
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
   })
-})
