8a644c6f2800180586677e796984e2d4a78b1e20
Wojtek Mach <wojtek@wojtekmach.pl>
2021-08-19 12:28:30 +0200

    Use elixir executable to print the version, closes #70

--- src/install-elixir.sh
+++ src/install-elixir.sh
@@ -13,4 +13,4 @@ wget -q -O "${FILE_OUTPUT}" "https://repo.hex.pm/builds/elixir/${FILE_INPUT}"
 mkdir -p "${DIR_FOR_BIN}"
 unzip -q -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}"
 echo "Installed Elixir version follows"
-${DIR_FOR_BIN}/bin/iex -v
+${DIR_FOR_BIN}/bin/elixir -v
