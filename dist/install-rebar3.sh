9020a2da705aeb2bf334c9e45f67928353d5a0f1
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-01-12 11:44:33 +0000

    Fix as per remote CI

--- dist/install-rebar3.sh
+++ dist/install-rebar3.sh
@@ -10,12 +10,14 @@ FILE_OUTPUT=rebar3
 DIR_FOR_BIN=.setup-beam/rebar3
 
 REBAR3_TARGET="https://github.com/erlang/rebar3/releases/download/${VSN}/${FILE_INPUT}"
+REBAR3_NIGHTLY=""
 if [ "${VSN}" == "nightly" ]; then
     REBAR3_TARGET="https://s3.amazonaws.com/rebar3-nightly/rebar3"
+    REBAR3_NIGHTLY=" (from nightly build)"
 fi
 wget -q -O "${FILE_OUTPUT}" "${REBAR3_TARGET}"
 mkdir -p "${DIR_FOR_BIN}/bin"
 chmod +x "${FILE_OUTPUT}"
 mv "${FILE_OUTPUT}" "${DIR_FOR_BIN}/bin"
-echo "Installed rebar3 version follows"
+echo "Installed rebar3 version$REBAR3_NIGHTLY follows"
 ${DIR_FOR_BIN}/bin/rebar3 version
