e7f15f50f265141d878c939014160998e27a8713
Bryan Paxton <bryan@starbelly.io>
2022-04-21 10:29:21 -0500

    Use overwrite modifier on unzip to avoid prompts

--- dist/install-elixir.sh
+++ dist/install-elixir.sh
@@ -11,7 +11,7 @@ DIR_FOR_BIN=.setup-beam/elixir
 
 wget -q -O "${FILE_OUTPUT}" "https://repo.hex.pm/builds/elixir/${FILE_INPUT}"
 mkdir -p "${DIR_FOR_BIN}"
-unzip -q -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}"
+unzip -q -o -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}"
 echo "Installed Elixir version follows"
 ${DIR_FOR_BIN}/bin/elixir -v
 
