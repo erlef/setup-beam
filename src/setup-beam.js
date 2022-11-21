72ff1cbaffc1a16158bae841f14514991e254145
Blake Kostner <git@btkostner.io>
2022-11-21 11:27:35 -0700

    use os homedir for escript install path

--- src/setup-beam.js
+++ src/setup-beam.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const { exec } = require('@actions/exec')
+const os = require('os')
 const path = require('path')
 const semver = require('semver')
 const https = require('https')
@@ -70,7 +71,7 @@ async function maybeInstallElixir(elixirSpec, otpVersion) {
         `##[add-matcher]${path.join(matchersPath, 'elixir-matchers.json')}`,
       )
     }
-    core.addPath(`${process.env.HOME}/.mix/escripts`)
+    core.addPath(`${os.homedir()}/.mix/escripts`)
     core.addPath(`${process.env.RUNNER_TEMP}/.setup-beam/elixir/bin`)
     console.log('##[endgroup]')
 
