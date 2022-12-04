1d327ead3b648bdbc4c09c5bacd9736d93f5cdb7
Paul Guyot <pguyot@kallisys.net>
2022-12-04 11:02:23 +0100

    Rebuild generated files
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- dist/install-elixir.sh
+++ dist/install-elixir.sh
@@ -6,18 +6,13 @@ cd "${RUNNER_TEMP}"
 
 VSN=${1}
 HEX_MIRROR=${2}
-CACHEDIR="${RUNNER_TEMP}/.setup-beam/cache"
 FILE_INPUT="${VSN}.zip"
-FILE_OUTPUT="${CACHEDIR}/elixir.zip"
+FILE_OUTPUT=elixir.zip
 DIR_FOR_BIN=.setup-beam/elixir
 
-mkdir -p "${CACHEDIR}"
-[ -e "${FILE_OUTPUT}" ] || curl -sfL -o "${FILE_OUTPUT}" "${HEX_MIRROR}/builds/elixir/${FILE_INPUT}"
+wget -q -O "${FILE_OUTPUT}" "${HEX_MIRROR}/builds/elixir/${FILE_INPUT}"
 mkdir -p "${DIR_FOR_BIN}"
-unzip -q -o -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}" || {
-    rm "${FILE_OUTPUT}"
-    exit 1
-}
+unzip -q -o -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}"
 echo "Installed Elixir version follows"
 ${DIR_FOR_BIN}/bin/elixir -v
 
