2d314bc41389ed43737d2c8a1005faaea046520e
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-12-14 15:25:50 +0000

    Check behaviour on Windows pre- Gleam 0.23 (#166)
    * Check behaviour on Windows pre- Gleam 0.23
    
    * Increase consistency with .sh counterpart
    
    * Support older versions (pre 0.22.1) on Windows
    
    * Increase inter-script consistency
    
    * Run `npm run build-dist`
    
    * React to CI results

--- dist/install-gleam.ps1
+++ dist/install-gleam.ps1
@@ -4,9 +4,25 @@ $ErrorActionPreference="Stop"
 
 Set-Location ${Env:RUNNER_TEMP}
 
-$FILE_INPUT="gleam-${VSN}-x86_64-pc-windows-msvc.zip"
 $FILE_OUTPUT="gleam.zip"
 $DIR_FOR_BIN=".setup-beam/gleam"
+
+function Version-Greater-Than([string]${THIS_ONE}, [string]${REFERENCE}) {
+  $THIS_ONE=$THIS_ONE.replace('v', '')
+  $THIS_ONE=$THIS_ONE.replace('rc', '')
+  $THIS_ONE=$THIS_ONE.replace('-', '')
+  return [version]${THIS_ONE} -gt [version]${REFERENCE}
+}
+
+function Uses-LLVM-Triplets([string]${THIS_VSN}) {
+  return "${THIS_VSN}" -eq "nightly" -or (Version-Greater-Than "${THIS_VSN}" "0.22.1")
+}
+
+if (Uses-LLVM-Triplets "$VSN") {
+   $FILE_INPUT="gleam-${VSN}-x86_64-pc-windows-msvc.zip"
+} else {
+   $FILE_INPUT="gleam-${VSN}-windows-64bit.zip"
+}
 
 $ProgressPreference="SilentlyContinue"
 Invoke-WebRequest "https://github.com/gleam-lang/gleam/releases/download/${VSN}/${FILE_INPUT}" -OutFile "${FILE_OUTPUT}"
