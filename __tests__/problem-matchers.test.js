a255120a8298b1d3ebd952d84954db5471801f8f
Blake Kostner <git@btkostner.io>
2023-03-29 07:43:12 -0600

    Remove Dialyzer-specific problem matcher (#182)
    * fix: remove dialyzer problem matcher
    
    * remove dialyzer problem matcher test
    
    * remove dist problem matcher

--- __tests__/problem-matchers.test.js
+++ __tests__/problem-matchers.test.js
@@ -6,7 +6,6 @@ async function all() {
   await testElixirMixCompileWarning()
   await testElixirMixTestFailure()
   await testElixirCredoOutputDefault()
-  await testElixirDialyzerOutputDefault()
 }
 
 async function testElixirMixCompileError() {
@@ -72,24 +71,6 @@ async function testElixirCredoOutputDefault() {
   assert.equal(file, 'lib/test.ex')
   assert.equal(line, '15')
   assert.equal(column, '7')
-}
-
-async function testElixirDialyzerOutputDefault() {
-  const [messagePattern, filePattern] = problemMatcher.find(
-    ({ owner }) => owner === 'elixir-dialyzerOutputDefault',
-  ).pattern
-
-  const firstOutput = 'lib/test.ex:15:invalid_contract'
-  const secondOutput =
-    'The @spec for the function does not match the success typing of the function.'
-
-  const [, file, line, code] = firstOutput.match(messagePattern.regexp)
-  assert.equal(file, 'lib/test.ex')
-  assert.equal(line, '15')
-  assert.equal(code, 'invalid_contract')
-
-  const [, message] = secondOutput.match(filePattern.regexp)
-  assert.equal(message, secondOutput)
 }
 
 all()
